08865f7e8a4e8d3019a8efb604b13695bcfea208
MatheusNaoki <93060423+MatheusNaoki@users.noreply.github.com>
2024-07-28 14:36:28 -0300

    first commit

---
+++ src/test/java/com/digitalinnovation/gof/LabPadroesdeprojetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesdeprojetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
